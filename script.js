af1a74e289590e100f5c0ff0b7bbc05d20da0efa
harshal-hp-29 <harshalparihar2905@gmail.com>
2025-08-12 01:34:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 10) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
 
